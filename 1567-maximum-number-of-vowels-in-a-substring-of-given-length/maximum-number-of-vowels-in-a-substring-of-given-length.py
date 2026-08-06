d3334391edd12ca50f88e701cbe609754e2b7973
Vagdevi11 <vagdevirsld@gmail.com>
2026-08-06 12:03:05 +0530

    Time: 63 ms (35.75%) | Memory: 19.7 MB (91.16%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,5 +1,8 @@
+def is_v(ch):
+    return ch in "aeiou"
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
+        """
         l=list(s)
         left=0
         count=0
@@ -12,6 +15,22 @@ class Solution:
                 if s[left] in "aeiou" :
                     count-=1
                 left+=1
-        return max_count        
+        return max_count  
+        """
+        first_window=s[:k]
+        v_c=0
+        for i in first_window:
+            if is_v(i):
+                v_c+=1
+        mx_v=max(v_c,0)
+        #sliding window logic
+        for i in range(k,len(s)):
+            if is_v(s[i]):
+                v_c+=1
+            if is_v(s[i-k]):
+                v_c-=1
+            mx_v=max(mx_v,v_c)
+        return mx_v                   
+
 
         
