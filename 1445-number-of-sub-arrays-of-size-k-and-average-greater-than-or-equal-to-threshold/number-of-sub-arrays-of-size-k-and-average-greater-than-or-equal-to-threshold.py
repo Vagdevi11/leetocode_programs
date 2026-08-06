5cddf6b509be8222e4b5af9875b6b1fcc9e38802
Vagdevi11 <vagdevirsld@gmail.com>
2026-08-06 11:01:06 +0530

    Time: 47 ms (55.92%) | Memory: 31 MB (7.91%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,5 +1,6 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        """
         left=0
         sm=0
         count=0
@@ -11,7 +12,19 @@ class Solution:
                     count+=1
                 sm-=arr[left]
                 left+=1
-        return count        
+        return count 
+        """
+        #another approach of sliding window concept
+        first_window=arr[:k]
+        count=0
+        current_sm=sum(first_window)
+        if current_sm/k>=threshold:
+            count+=1    
+        for i in range(k,len(arr)):
+            current_sm=current_sm+arr[i]-arr[i-k]
+            if current_sm/k>=threshold:
+                count+=1
+        return count               
 
 
 
